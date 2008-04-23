c47877308c5b1bcc8a64a34d38d502696b222237
Ian Graeme Hilt <ian.hilt@gmail.com>
2008-04-23 00:20:26 -0400

    My timer project
    Signed-off-by: Ian Graeme Hilt <ian.hilt@gmail.com>

--- ppt.c
+++ ppt.c
@@ -1,35 +1,77 @@
-/*
- * example.c: very simple example of port I/O
- *
- * This code does nothing useful, just a port write, a pause,
- * and a port read. Compile with `gcc -O2 -o example example.c',
- * and run as root with `./example'.
- */
-
 #include <stdio.h>
-#include <unistd.h>
-#include <asm/io.h>
+#include <stdlib.h>
+#include <time.h>
+#include </usr/src/linux/include/asm/io.h>
 
-#define BASEPORT 0x378 /* lp1 */
+#define BASEPORT	0x378
+#define STATUS		BASEPORT+1
+#define CONTROL		BASEPORT+2
+#define ON			1
+#define OFF			0
 
 int main()
 {
-  /* Get access to the ports */
-  if (ioperm(BASEPORT, 3, 1)) {perror("ioperm"); exit(1);}
-  
-  /* Set the data signals (D0-7) of the port to all low (0) */
-  outb(0, BASEPORT);
-  
-  /* Sleep for a while (100 ms) */
-  usleep(100000);
-  
-  /* Read from the status port (BASE+1) and display the result */
-  printf("status: %d\n", inb(BASEPORT + 1));
+	int status; /* Container for STATUS bits 3 - 7 */
 
-  /* We don't need the ports anymore */
-  if (ioperm(BASEPORT, 3, 0)) {perror("ioperm"); exit(1);}
+/* Flow control variables */
+	char BUSY_STATE, ACK_STATE, ERR_STATE, SLCT_STATE, PE_STATE = OFF;
 
-  exit(0);
+/* setup structure for nanosleep() */
+	struct timespec req = {0};
+	req.tv_nsec = 1000000;
+
+	if (ioperm(BASEPORT, 3, 1)) {
+		perror("ioperm");
+		exit(1);
+	}
+
+	printf("status: %x\n", inb(STATUS));
+
+	while (status = (inb(STATUS) >> 3)^0x10) {
+		nanosleep(&req, NULL);
+		if((~(status >> 4) & 0x01) == ON && (BUSY_STATE != ON)) {
+			printf("BUSY ON\n");
+			BUSY_STATE = ON;
+		}
+		else if((~(status >> 4) & 0x01) == OFF && BUSY_STATE == ON) {
+			printf("BUSY OFF\n");
+			BUSY_STATE = OFF;
+		}
+		else if((~(status >> 3) & 0x01) == ON && (ACK_STATE != ON)) {
+			printf("ACK ON\n");
+			ACK_STATE = ON;
+		}
+		else if((~(status >> 3) & 0x01) == OFF && ACK_STATE == ON) {
+			printf("ACK OFF\n");
+			ACK_STATE = OFF;
+		}
+		else if((~(status >> 1) & 0x01) == ON && (SLCT_STATE != ON)) {
+			printf("SLCT ON\n");
+			SLCT_STATE = ON;
+		}
+		else if((~(status >> 1) & 0x01) == OFF && SLCT_STATE == ON) {
+			printf("SLCT OFF\n");
+			SLCT_STATE = OFF;
+		}
+		else if(~(status & 0x01) == ON && (ERR_STATE != ON)) {
+			printf("ERR ON\n");
+			ERR_STATE = ON;
+		}
+		else if(~(status & 0x01) == OFF && ERR_STATE == ON){
+			ERR_STATE = OFF;
+		}
+		else if((~(status >> 2) & 0x01) == ON && (PE_STATE != ON)) {
+			printf("PE ON\n");
+			PE_STATE = ON;
+		}
+		else if((~(status >> 2) & 0x01) == OFF && PE_STATE == ON) {
+			printf("PE OFF\n");
+			PE_STATE = OFF;
+		}
+	}
+	if (ioperm(BASEPORT, 3, 0)) {
+		perror("ioperm");
+		exit(1);
+	}
+	exit(0);
 }
-
-/* end of example.c */
