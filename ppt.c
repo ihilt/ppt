80fe93bb8935e58c54908792facb4e1bb9cf1f10
Ian Graeme Hilt <ian.hilt@gmail.com>
2008-04-29 10:37:51 -0400

    Introduced functions to handle output
    output_on_time() and output_off_time() handle the output from the while loop to
    the file. I think this will help condense the body of the program and improve
    the logical flow of the code.
    
    I collected all of the signal names into const char's to preclude the need to
    enlarge the while loop and/or the output functions. As is stated in the comment
    for these, there may be a better way to do this; not sure yet.
    
    Signed-off-by: Ian Graeme Hilt <ian.hilt@gmail.com>

--- ppt.c
+++ ppt.c
@@ -6,8 +6,20 @@
 #define BASEPORT	0x378
 #define STATUS		BASEPORT+1
 #define CONTROL		BASEPORT+2
-#define ON			1
-#define OFF			0
+#define ON		1
+#define OFF		0
+
+/*
+ * These functions do what they say they do.
+ *
+ * output_on_time() sends the timestamp to fp with the name of the signal and
+ * the "ON" string.
+ *
+ * output_off_time() sends the timestamp to fp with the name of the signal and
+ * the "OFF" string.
+ */
+void output_on_time(FILE *, const char fname[]);
+void output_off_time(FILE *, const char fname[]);
 
 int main()
 {
@@ -28,73 +40,63 @@ int main()
 		exit(1);
 	}
 
+/*
+ * There might be a better way to do this, but for now I've collected all of
+ * the signal name strings here.
+ */
+	const char busy[] = "BUSY";
+	const char ack[] = "ACK";
+	const char slct[] = "SLCT";
+	const char err[] = "ERR";
+	const char pe[] = "PE";
+
 	printf("status: %x\n", inb(STATUS));
 
 	while (status = (inb(STATUS) >> 3)^0x10) {
 		nanosleep(&req, NULL);
 		/* BUSY */
 		if((~(status >> 4) & 0x01) == ON && (BUSY_STATE != ON)) {
-			fp = fopen("BUSY", "a");
-			fprintf(fp, "BUSY ON, %d\n", time(NULL));
-			fclose(fp);
+			output_on_time(fp, busy);
 			BUSY_STATE = ON;
 		}
 		else if((~(status >> 4) & 0x01) == OFF && BUSY_STATE == ON) {
-			fp = fopen("BUSY", "a");
-			fprintf(fp, "BUSY OFF, %d\n", time(NULL));
-			fclose(fp);
+			output_off_time(fp, busy);
 			BUSY_STATE = OFF;
 		}
 		/* ACK */
 		else if((~(status >> 3) & 0x01) == ON && (ACK_STATE != ON)) {
-			fp = fopen("ACK", "a");
-			fprintf(fp, "ACK ON, %d\n", time(NULL));
-			fclose(fp);
+			output_on_time(fp, ack);
 			ACK_STATE = ON;
 		}
 		else if((~(status >> 3) & 0x01) == OFF && ACK_STATE == ON) {
-			fp = fopen("ACK", "a");
-			fprintf(fp, "ACK OFF, %d\n", time(NULL));
-			fclose(fp);
+			output_off_time(fp, ack);
 			ACK_STATE = OFF;
 		}
 		/* SLCT */
 		else if((~(status >> 1) & 0x01) == ON && (SLCT_STATE != ON)) {
-			fp = fopen("SLCT", "a");
-			fprintf(fp, "SLCT ON, %d\n", time(NULL));
-			fclose(fp);
+			output_on_time(fp, slct);
 			SLCT_STATE = ON;
 		}
 		else if((~(status >> 1) & 0x01) == OFF && SLCT_STATE == ON) {
-			fp = fopen("SLCT", "a");
-			fprintf(fp, "SLCT OFF, %d\n", time(NULL));
-			fclose(fp);
+			output_off_time(fp, slct);
 			SLCT_STATE = OFF;
 		}
 		/* ERR */
 		else if(~(status & 0x01) == ON && (ERR_STATE != ON)) {
-			fp = fopen("ERR", "a");
-			fprintf(fp, "ERR ON, %d\n", time(NULL));
-			fclose(fp);
+			output_on_time(fp, err);
 			ERR_STATE = ON;
 		}
 		else if(~(status & 0x01) == OFF && ERR_STATE == ON) {
-			fp = fopen("ERR", "a");
-			fprintf(fp, "ERR OFF, %d\n", time(NULL));
-			fclose(fp);
+			output_off_time(fp, err);
 			ERR_STATE = OFF;
 		}
 		/* PE */
 		else if((~(status >> 2) & 0x01) == ON && (PE_STATE != ON)) {
-			fp = fopen("PE", "a");
-			fprintf(fp, "PE ON, %d\n", time(NULL));
-			fclose(fp);
+			output_on_time(fp, pe);
 			PE_STATE = ON;
 		}
 		else if((~(status >> 2) & 0x01) == OFF && PE_STATE == ON) {
-			fp = fopen("PE", "a");
-			fprintf(fp, "PE OFF, %d\n", time(NULL));
-			fclose(fp);
+			output_off_time(fp, pe);
 			PE_STATE = OFF;
 		}
 	}
@@ -103,4 +105,18 @@ int main()
 		exit(1);
 	}
 	exit(0);
+}
+
+void output_on_time(FILE *fp, const char fname[])
+{
+	fp = fopen(fname, "a");
+	fprintf(fp, "%s ON, %d\n", fname, time(NULL));
+	fclose(fp);
+}
+
+void output_off_time(FILE *fp, const char fname[])
+{
+	fp = fopen(fname, "a");
+	fprintf(fp, "%s OFF, %d\n", fname, time(NULL));
+	fclose(fp);
 }
